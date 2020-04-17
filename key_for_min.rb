7b5fae993efeaa3d668eb58d784106f3fbe43e82
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-17 11:38:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,12 @@
 def key_for_min_value(name_hash)
   lowest_val = 0 
   result_key = nil
-  name_hash.collect do |key, val|
-    
+  name_hash.each do |key, val|
+    if lowest_val == 0 || value < lowest_val
+      lowest_val = value
+      result_key = key
     end
   end
+  
+  result_key
 end
